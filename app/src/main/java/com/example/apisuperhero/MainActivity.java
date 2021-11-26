7f78789e57115a8b8b1925959896fe0df6b1bf14
José Carlos Valeriano Rodríguez <78151884+JoseCarlosValeriano@users.noreply.github.com>
2021-11-26 11:35:11 +0000

    Creado Activity_main, main.xml, string.xml

--- app/src/main/java/com/example/apisuperhero/MainActivity.java
+++ app/src/main/java/com/example/apisuperhero/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.apisuperhero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.MediaRouteButton;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.apisuperhero.utils.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView urlDisplay;
+    TextView searchResults;
+
+
+    public class SuperHeroQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            MediaRouteButton requestProgress;
+            requestProgress.setVisibility(View.VISIBLE);
+        }
+
+
+
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            return null;
+        }
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String gitHubSearchResults = null;
+
+            try {
+                gitHubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return gitHubSearchResults;
+        }
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        urlDisplay = (TextView) findViewById(R.id.url_display);
+        searchResults = (TextView) findViewById(R.id.github_search_result);
+
     }
 }
--- app/src/main/java/com/example/apisuperhero/utils/NetworkUtils.java
+++ app/src/main/java/com/example/apisuperhero/utils/NetworkUtils.java
@@ -1,4 +1,8 @@
 package com.example.apisuperhero.utils;
 
+import java.net.URL;
+
 public class NetworkUtils {
+    public static String getResponseFromHttpUrl(URL searchUrl) {
+    }
 }
